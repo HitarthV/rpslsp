448c8002997e511e41fbff66b1eb1f5582031124
HitsPsych <129500306+HitsPsych@users.noreply.github.com>
2023-04-16 19:02:24 +0100

    Add files via upload

---
+++ rpslsp/rpslsp/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace rpslsp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Computer:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.ForeColor = System.Drawing.Color.Blue;
+            this.scoreLabel.Location = new System.Drawing.Point(35, 56);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(38, 13);
+            this.scoreLabel.TabIndex = 7;
+            this.scoreLabel.Text = "Score:";
+            this.scoreLabel.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "WELCOME TO ";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.BackgroundImage = global::rpslsp.Properties.Resources.download__4_;
+            this.groupBox5.Location = new System.Drawing.Point(298, 211);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(191, 193);
+            this.groupBox5.TabIndex = 15;
+            this.groupBox5.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackgroundImage = global::rpslsp.Properties.Resources.download__3_;
+            this.groupBox4.Location = new System.Drawing.Point(747, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(191, 193);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackgroundImage = global::rpslsp.Properties.Resources.download__2_;
+            this.groupBox3.Location = new System.Drawing.Point(959, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(191, 193);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackgroundImage = global::rpslsp.Properties.Resources.download__1_;
+            this.groupBox2.Location = new System.Drawing.Point(504, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 195);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = global::rpslsp.Properties.Resources.download;
+            this.groupBox1.Location = new System.Drawing.Point(298, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 193);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(137, 56);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(40, 13);
+            this.resultLabel.TabIndex = 16;
+            this.resultLabel.Text = "Result:";
+            this.resultLabel.Click += new System.EventHandler(this.resultLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(1231, 465);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ rpslsp/rpslsp/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rpslsp
+{
+    public partial class Form1 : Form
+    {
+        private int playerScore = 0;
+        private int computerScore = 0;
+        private string[] choices = { "rock", "paper", "scissors", "lizard", "spock" };
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scoreLabel.Text = "Player: 0 | Computer: 0";
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string playerChoice = ((TextBox)sender).Tag.ToString();
+            string computerChoice = choices[random.Next(choices.Length)];
+
+            switch (playerChoice)
+            {
+                case "rock":
+                    if (computerChoice == "scissors" || computerChoice == "lizard")
+                    {
+                        playerScore++;
+                        resultLabel.Text = "You win!";
+                    }
+                    else if (computerChoice == "paper" || computerChoice == "spock")
+                    {
+                        computerScore++;
+                        resultLabel.Text = "Computer wins!";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "It's a tie!";
+                    }
+                    break;
+                case "paper":
+                    if (computerChoice == "rock" || computerChoice == "spock")
+                    {
+                        playerScore++;
+                        resultLabel.Text = "You win!";
+                    }
+                    else if (computerChoice == "scissors" || computerChoice == "lizard")
+                    {
+                        computerScore++;
+                        resultLabel.Text = "Computer wins!";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "It's a tie!";
+                    }
+                    break;
+                case "scissors":
+                    if (computerChoice == "paper" || computerChoice == "lizard")
+                    {
+                        playerScore++;
+                        resultLabel.Text = "You win!";
+                    }
+                    else if (computerChoice == "rock" || computerChoice == "spock")
+                    {
+                        computerScore++;
+                        resultLabel.Text = "Computer wins!";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "It's a tie!";
+                    }
+                    break;
+                case "lizard":
+                    if (computerChoice == "paper" || computerChoice == "spock")
+                    {
+                        playerScore++;
+                        resultLabel.Text = "You win!";
+                    }
+                    else if (computerChoice == "rock" || computerChoice == "scissors")
+                    {
+                        computerScore++;
+                        resultLabel.Text = "Computer wins!";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "It's a tie!";
+                    }
+                    break;
+                case "spock":
+                    if (computerChoice == "rock" || computerChoice == "scissors")
+                    {
+                        playerScore++;
+                        resultLabel.Text = "You win!";
+                    }
+                    else if (computerChoice == "paper" || computerChoice == "lizard")
+                    {
+                        computerScore++;
+                        resultLabel.Text = "Computer wins!";
+                    }
+                    else
+                    {
+                        resultLabel.Text = "It's a tie!";
+                    }
+                    break;
+            }
+           scoreLabel.Text = $"Player: {playerScore} | Computer: {computerScore}";
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
